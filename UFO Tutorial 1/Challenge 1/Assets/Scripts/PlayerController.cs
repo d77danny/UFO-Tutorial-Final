4158b55fe0597cc42a7cbfe0be0f5a8acd2d021a
d77danny <54646874+d77danny@users.noreply.github.com>
2019-09-22 18:13:19 -0400

    Challenge 1

---
+++ UFO Tutorial 1/Challenge 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text lossText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        lossText.text = "";
+        SetCountText();
+        SetLivesText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+            lives = lives - 1;
+            SetLivesText();
+        }
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        if (count == 12)
+        {
+            transform.position = new Vector2(80.5f, 60.5f);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "YouWin! Game created by Daniel Garcia";
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            lossText.text = "You Lose... Game created by Daniel Garcia";
+            Destroy(gameObject);
+        }
+
+    }
+}
